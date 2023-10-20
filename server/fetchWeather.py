a448643fb9a8807adf204548392365b7f6b3512b
Ali Amin <aliamin745@gmail.com>
2023-10-20 19:56:51 -0400

    added gitignore

--- server/fetchWeather.py
+++ server/fetchWeather.py
@@ -1,10 +1,9 @@
 import requests
-
-api_key = "cadcac0a6f0d7364dcb0d91ac3622abb"
+import creds
 
 
 def fetchData(city):
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={creds.api_key}"
 
     response = requests.get(url)
     if response.status_code == 200:
