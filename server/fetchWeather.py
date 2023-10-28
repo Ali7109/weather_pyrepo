35e32713119e3f7eff2b38be0498d1f7714f7e3c
Ali Amin <aliamin745@gmail.com>
2023-10-28 13:06:57 -0400

    added features

--- server/fetchWeather.py
+++ server/fetchWeather.py
@@ -2,8 +2,30 @@ import requests
 import creds
 
 
-def fetchData(city):
+def get_current_weather(city):
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={creds.api_key}"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return -1
+
+
+def get_daily_forecast(city, num_days=5):
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={creds.api_key}&cnt={num_days * 8}"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return -1
+
+
+def get_weekly_forecast(city):
+    url = f"https://api.openweathermap.org/data/2.5/forecast/daily?q={city}&appid={creds.api_key}&cnt=7"
 
     response = requests.get(url)
     if response.status_code == 200:
